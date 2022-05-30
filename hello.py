4315d44240db79b5525bc890dc04afadff252b81
Rodolfo Matos <rodolfojmatos@gmail.com>
2022-05-30 16:28:54 -0400

    Implemented Hello World multi languages

--- hello.py
+++ hello.py
@@ -1,4 +1,31 @@
 #!usr/bin/env python3
+"""
+Hello World Multi Languages
 
-# #This is a comment
-print('Hello World')  # test-ignore
+Depending on the setup language of your environmental variables the program 
+prints the corresponding message
+
+Usage:
+Have the Lang variable configured:
+
+    export LANG=pt_BR
+
+Execution:
+python3 hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Rodolfo Matos"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mundo!"
+
+
+print(msg)  # test-ignore
